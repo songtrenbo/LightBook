ccf80e8d4fe43c0a7ed57d9b56310342dee3ea1c
Nguyen Ly <songtrenbo1@gmail.com>
2022-04-12 20:24:13 +0700

    create view for SplashPage and SigninPage

---
+++ lightbook_flutter/lib/homepage/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:lightbook_flutter/homepage/components/body.dart';
+
+
+class HomePage extends StatelessWidget {
+  int selectIndex = 0;
+  static String routeName = "/home_screen";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white // This is optional
+    );
+  }
+}
