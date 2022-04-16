bafc7891dc562f6c96a32a8b2ceb9ce38db48299
minhchi32 <tranminhchi0302@gmail.com>
2022-04-16 08:13:09 +0700

    remove unnecessary file

--- .history/lightbook_flutter/lib/resources/auth_methods_20220414174023.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-class AuthMethods {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  
-}
--- .history/lightbook_flutter/lib/utils/utils_20220415124632.dart
+++
@@ -1,8 +0,0 @@
-// for displaying snackbars
-showSnackBar(BuildContext context, String text) {
-  return ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(text),
-    ),
-  );
-}
